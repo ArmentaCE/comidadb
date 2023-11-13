e4765acf7af67f81883c324ae0fe8184b87232dd
Martin Garcia Guia <martin_sillo7@hotmail.com>
2023-11-13 11:38:19 -0600

    Componente terminado de busqueda por ingrediente

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Token } from './components/Token';
 import { Category } from './components/Category'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ingredient } from './components/Ingedient';
 
 export default function App() {
 
@@ -15,14 +16,14 @@ export default function App() {
       <StatusBar style="auto" /> */}
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='Login' component={Login}/>
-          <Stack.Screen name='Token' component={Token}/>
+          {/* <Stack.Screen name='Login' component={Login}/>
+          <Stack.Screen name='Token' component={Token}/> */}
 
           {/* Rutas de la comida por nombre, categoria e ingrediente */}
           {/* IMPORTANTE cambiar el nombre del componente*/}
           {/* Sí cambiaste el nombre de la ruta asegurate de ir al componente "Token" y hacer el respectivo cambio para que el boton pueda encontrar dicha ruta */}
-          <Stack.Screen name='mealByName' component={Token}/>
-          <Stack.Screen name='mealByIngredient' component={Token}/>
+          {/* <Stack.Screen name='mealByName' component={Token}/> */}
+          <Stack.Screen name='mealByIngredient' component={Ingredient}/>
           <Stack.Screen name='mealByCategory' component={Category} options={{title: "Busqueda por categoria"}}/>
         </Stack.Navigator>
       </NavigationContainer>
